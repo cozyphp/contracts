6ef01acb5ecaf3c5d545c6fbec29131356e02146
Nestor Picado <info@nestorpicado.com>
2024-03-01 12:44:12 -0500

    update contracts

--- src/Arrayable.php
+++ src/Arrayable.php
@@ -3,8 +3,6 @@
 /**
  * Cozy Contracts
  *
- * (c) Nestor Picado <info@nestorpicado.com>
- *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
