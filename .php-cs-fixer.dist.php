efadde84a7ee6447f93ddc69fe59ede90c20775d
Nestor Picado <info@nestorpicado.com>
2024-03-01 12:44:05 -0500

    update manifests

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,8 +3,6 @@
 $header = <<<EOF
     Cozy Contracts
 
-    (c) Nestor Picado <info@nestorpicado.com>
-
     For the full copyright and license information, please view the LICENSE
     file that was distributed with this source code.
     EOF;
